253798aecd91afe7ba0e3b783632199858258d11
josh098741 <morongemokaya@gmail.com>
2026-01-06 23:43:15 +0300

    Implement product routes and controller for CRUD operations

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import * as productController from "../controllers/productController"
+import { requireAuth } from "@clerk/express"
 
 const router = Router()
 
-
+// GET /api/products => Get all the products (pubic)
+router.get("/", productController.getAllProducts)
 
 export default router
