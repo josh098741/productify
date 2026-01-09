6876ace1ac032bfdba6b1538209ef68b06a2050c
josh098741 <morongemokaya@gmail.com>
2026-01-09 10:22:43 +0300

    feat: add authentication checks for profile and edit routes, integrate SignInButton in HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,9 +22,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/profile" element={isSignedIn ? <ProfilePage/> : <Navigate to="/" />} />
           <Route path="/create" element={isSignedIn ? <CreatePage /> : <Navigate to="/" />} />
-          <Route path="/edit/:id" element={<EditProfilePage />} />
+          <Route path="/edit/:id" element={isSignedIn ? <EditProfilePage /> : <Navigate to="/" />} />
         </Routes>
       </main>
     </div>
