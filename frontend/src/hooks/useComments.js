95ce4a5b40f622d3fa42fdb36069ef1c464b3bc9
josh098741 <morongemokaya@gmail.com>
2026-01-10 13:53:25 +0300

    Creation of comment creation

---
+++ frontend/src/hooks/useComments.js
@@ -0,0 +1,14 @@
+import { useMutation } from "@tanstack/react-query"
+import { createComment, deleteComment } from "../lib/api"
+
+export const useCreateComment = () => {
+    return useMutation({
+        mutationFn: createComment
+    })
+}
+
+export const useDeleteComment = () => {
+    return useMutation({
+        mutationFn: deleteComment
+    })
+}
