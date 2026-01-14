426feae44089efe8f5ff67ccc3a9f12f7c9b6a70
josh098741 <morongemokaya@gmail.com>
2026-01-14 23:12:18 +0300

    _

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,15 +1,15 @@
 import { useMutation, useQuery } from "@tanstack/react-query"
 import { createProduct, deleteProduct, getAllProducts, getMyProducts, getProductById } from "../lib/api"
-
+                 
 export const useProducts = () => {
     const result = useQuery({
         queryKey: ["products"],
         queryFn: getAllProducts
     })
-
+         
     return result
 }
-
+              
 export const useCreateProduct = () => {
     return useMutation({
         mutationFn: createProduct
